8fbae0ea3aa62941956dbd2698901e25739534ce
Olina Stathopoulou <olina.stath@nyu.edu>
2020-10-11 04:37:04 +0100

    added init filters

--- app.js
+++ app.js
@@ -6,16 +6,16 @@
  * For more information, read
  * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
  */
-const express = require('express'); // Express web server framework
-const request = require('request'); // "Request" library
-const cors = require('cors');
-const querystring = require('querystring');
-const cookieParser = require('cookie-parser');
-const { config } = require('./config');
+const express = require("express"); // Express web server framework
+const request = require("request"); // "Request" library
+const cors = require("cors");
+const querystring = require("querystring");
+const cookieParser = require("cookie-parser");
+const config = require("./config");
 
-var client_id = config.clientID; // Your client id
+var client_id = config.clientId; // Your client id
 var client_secret = config.clientSecret; // Your secret
-var redirect_uri = 'http://localhost:3000/callback'; // Your redirect uri
+var redirect_uri = config.redirectUri; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
@@ -23,8 +23,9 @@ var redirect_uri = 'http://localhost:3000/callback'; // Your redirect uri
  * @return {string} The generated string
  */
 var generateRandomString = function (length) {
-	var text = '';
-	var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	var text = "";
+	var possible =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
 	for (var i = 0; i < length; i++) {
 		text += possible.charAt(Math.floor(Math.random() * possible.length));
@@ -32,33 +33,34 @@ var generateRandomString = function (length) {
 	return text;
 };
 
-var stateKey = 'spotify_auth_state';
+var stateKey = "spotify_auth_state";
 
 var app = express();
 
-app.use(express.static(__dirname + '/public'))
+app
+	.use(express.static(__dirname + "/public"))
 	.use(cors())
 	.use(cookieParser());
 
-app.get('/login', function (req, res) {
+app.get("/login", function (req, res) {
 	var state = generateRandomString(16);
 	res.cookie(stateKey, state);
 
 	// your application requests authorization
-	var scope = 'user-read-private user-read-email user-top-read';
+	var scope = "user-read-private user-read-email user-top-read";
 	res.redirect(
-		'https://accounts.spotify.com/authorize?' +
+		"https://accounts.spotify.com/authorize?" +
 			querystring.stringify({
-				response_type: 'code',
+				response_type: "code",
 				client_id: client_id,
 				scope: scope,
 				redirect_uri: redirect_uri,
-				state: state
+				state: state,
 			})
 	);
 });
 
-app.get('/callback', function (req, res) {
+app.get("/callback", function (req, res) {
 	// your application requests refresh and access tokens
 	// after checking the state parameter
 
@@ -68,24 +70,26 @@ app.get('/callback', function (req, res) {
 
 	if (state === null || state !== storedState) {
 		res.redirect(
-			'/#' +
+			"/#" +
 				querystring.stringify({
-					error: 'state_mismatch'
+					error: "state_mismatch",
 				})
 		);
 	} else {
 		res.clearCookie(stateKey);
 		var authOptions = {
-			url: 'https://accounts.spotify.com/api/token',
+			url: "https://accounts.spotify.com/api/token",
 			form: {
 				code: code,
 				redirect_uri: redirect_uri,
-				grant_type: 'authorization_code'
+				grant_type: "authorization_code",
 			},
 			headers: {
-				Authorization: 'Basic ' + new Buffer(client_id + ':' + client_secret).toString('base64')
+				Authorization:
+					"Basic " +
+					new Buffer(client_id + ":" + client_secret).toString("base64"),
 			},
-			json: true
+			json: true,
 		};
 
 		request.post(authOptions, function (error, response, body) {
@@ -94,37 +98,37 @@ app.get('/callback', function (req, res) {
 					refresh_token = body.refresh_token;
 
 				var options = {
-					url: 'https://api.spotify.com/v1/me',
-					headers: { Authorization: 'Bearer ' + access_token },
-					json: true
+					url: "https://api.spotify.com/v1/me",
+					headers: { Authorization: "Bearer " + access_token },
+					json: true,
 				};
 
 				var topOptions = {
-					url: 'https://api.spotify.com/v1/me/top/tracks',
-					headers: { Authorization: 'Bearer ' + access_token },
-					json: true
+					url: "https://api.spotify.com/v1/me/top/tracks",
+					headers: { Authorization: "Bearer " + access_token },
+					json: true,
 				};
 
 				// use the access token to access the Spotify Web API
 				request.get(topOptions, function (error, response, body) {
 					for (let i = 0; i < body.items.length; i++) {
-						console.log(body);
+						// if (i < 10) console.log(body.items[i].album);
 					}
 				});
 
 				// we can also pass the token to the browser to make requests from there
 				res.redirect(
-					'/#' +
+					"/#" +
 						querystring.stringify({
 							access_token: access_token,
-							refresh_token: refresh_token
+							refresh_token: refresh_token,
 						})
 				);
 			} else {
 				res.redirect(
-					'/#' +
+					"/#" +
 						querystring.stringify({
-							error: 'invalid_token'
+							error: "invalid_token",
 						})
 				);
 			}
@@ -132,29 +136,33 @@ app.get('/callback', function (req, res) {
 	}
 });
 
-app.get('/refresh_token', function (req, res) {
+app.get("/refresh_token", function (req, res) {
 	// requesting access token from refresh token
 	var refresh_token = req.query.refresh_token;
 	var authOptions = {
-		url: 'https://accounts.spotify.com/api/token',
-		headers: { Authorization: 'Basic ' + new Buffer(client_id + ':' + client_secret).toString('base64') },
-		form: {
-			grant_type: 'refresh_token',
-			refresh_token: refresh_token
+		url: "https://accounts.spotify.com/api/token",
+		headers: {
+			Authorization:
+				"Basic " +
+				new Buffer(client_id + ":" + client_secret).toString("base64"),
 		},
-		json: true
+		form: {
+			grant_type: "refresh_token",
+			refresh_token: refresh_token,
+		},
+		json: true,
 	};
 
 	request.post(authOptions, function (error, response, body) {
 		if (!error && response.statusCode === 200) {
 			var access_token = body.access_token;
 			res.send({
-				access_token: access_token
+				access_token: access_token,
 			});
 		}
 	});
 });
 
 app.listen(process.env.PORT || 3000, function () {
-	console.log('Server is running on port 3000');
+	console.log("Server is running on port 3000");
 });
--- public/server.js
+++ public/server.js
@@ -4,13 +4,19 @@
 	 * @return Object
 	 */
 
-	var displayName = 'RECEIPTIFY';
-	var dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+	var displayName = "RECEIPTIFY";
+	var dateOptions = {
+		weekday: "long",
+		year: "numeric",
+		month: "long",
+		day: "numeric",
+	};
 	var today = new Date();
 
-	var userProfileSource = document.getElementById('user-profile-template').innerHTML,
+	var userProfileSource = document.getElementById("user-profile-template")
+			.innerHTML,
 		userProfileTemplate = Handlebars.compile(userProfileSource),
-		userProfilePlaceholder = document.getElementById('receipt');
+		userProfilePlaceholder = document.getElementById("receipt");
 
 	function getHashParams() {
 		var hashParams = {};
@@ -23,69 +29,155 @@
 		return hashParams;
 	}
 
+	function toCamelCase(str) {
+		return str.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
+	}
+
+	function populateAndDisplayNode(type, map) {
+		const node = document.getElementById(type);
+		node.innerHTML = "";
+
+		for (let i = 0; i < Object.keys(map).length; i++) {
+			let key = Object.keys(map)[i];
+			let name = map[key];
+			let value = map[key].split(" - ")[0].trim();
+
+			let divElement = document.createElement("DIV");
+			divElement.className = "hide-element";
+
+			let inputElement = document.createElement("INPUT");
+			inputElement.setAttribute("type", "checkbox");
+			inputElement.setAttribute("name", toCamelCase(name));
+			inputElement.setAttribute("id", toCamelCase(name));
+			inputElement.setAttribute("value", value);
+			divElement.appendChild(inputElement);
+
+			let labelElement = document.createElement("LABEL");
+			labelElement.setAttribute("for", toCamelCase(name));
+			labelElement.innerText = name;
+			divElement.appendChild(labelElement);
+			
+			node.appendChild(divElement);
+		}
+
+		const hideOptionsChildren = document.getElementsByClassName("hide-options-row")[1].children;
+		for (let i = 0; i < hideOptionsChildren.length; i++) {
+			if (hideOptionsChildren[i].id !== type) {
+				hideOptionsChildren[i].style.display = 'none';
+			} else {
+				hideOptionsChildren[i].style.display = 'flex';
+			}
+		}
+	}
+
+	function loadInitialTracks() {
+		const timeRangeSlugs = ["short_term", "medium_term", "long_term"];
+		const artists = {};
+		const albums = {};
+		timeRangeSlugs.forEach(timeRangeSlug => {
+			$.ajax({
+				url: `https://api.spotify.com/v1/me/top/tracks?limit=25&time_range=${timeRangeSlug}`,
+				headers: {
+					Authorization: "Bearer " + access_token,
+				},
+				async: false,
+				success: function (response) {
+					response.items.forEach(track => {
+						if (!albums[track.album.name]) {
+							albums[track.album.name] = `${track.album.name} - ${track.album.artists[0].name}`;
+						}
+
+						for (let i = 0; i < track.artists.length; i++) {
+							if (!artists[track.artists[i].name]) {
+								artists[track.artists[i].name] = track.artists[i].name;
+							}
+						}
+					
+					});
+				},
+				error: function (XMLHttpRequest, textStatus, errorThrown) {
+					// error handler here
+				},
+			});
+		});
+
+		document.getElementById("artists_btn").addEventListener("click", () => populateAndDisplayNode("artists", artists));
+		document.getElementById("albums_btn").addEventListener("click", () => populateAndDisplayNode("albums", albums));
+	}
+
 	function retrieveTracks(timeRangeSlug, domNumber, domPeriod) {
 		$.ajax({
 			url: `https://api.spotify.com/v1/me/top/tracks?limit=10&time_range=${timeRangeSlug}`,
 			headers: {
-				Authorization: 'Bearer ' + access_token
+				Authorization: "Bearer " + access_token,
 			},
 			success: function (response) {
 				var data = {
 					trackList: response.items,
 					total: 0,
-					date: today.toLocaleDateString('en-US', dateOptions).toUpperCase(),
-					json: true
+					date: today.toLocaleDateString("en-US", dateOptions).toUpperCase(),
+					json: true,
 				};
 				for (var i = 0; i < data.trackList.length; i++) {
 					data.trackList[i].name = data.trackList[i].name.toUpperCase();
 					data.total += data.trackList[i].duration_ms;
 					let minutes = Math.floor(data.trackList[i].duration_ms / 60000);
-					let seconds = ((data.trackList[i].duration_ms % 60000) / 1000).toFixed(0);
-					data.trackList[i].duration_ms = minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+					let seconds = (
+						(data.trackList[i].duration_ms % 60000) /
+						1000
+					).toFixed(0);
+					data.trackList[i].duration_ms =
+						minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
 					for (var j = 0; j < data.trackList[i].artists.length; j++) {
-						data.trackList[i].artists[j].name = data.trackList[i].artists[j].name.trim();
-						data.trackList[i].artists[j].name = data.trackList[i].artists[j].name.toUpperCase();
+						data.trackList[i].artists[j].name = data.trackList[i].artists[
+							j
+						].name.trim();
+						data.trackList[i].artists[j].name = data.trackList[i].artists[
+							j
+						].name.toUpperCase();
 						if (j != data.trackList[i].artists.length - 1) {
-							data.trackList[i].artists[j].name = data.trackList[i].artists[j].name + ', ';
+							data.trackList[i].artists[j].name =
+								data.trackList[i].artists[j].name + ", ";
 						}
 					}
 				}
 				minutes = Math.floor(data.total / 60000);
 				seconds = ((data.total % 60000) / 1000).toFixed(0);
-				data.total = minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+				data.total = minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
 				userProfilePlaceholder.innerHTML = userProfileTemplate({
 					tracks: data.trackList,
 					total: data.total,
 					time: data.date,
 					num: domNumber,
 					name: displayName,
-					period: domPeriod
+					period: domPeriod,
 				});
 
-				document.getElementById('download').addEventListener('click', function () {
-					var offScreen = document.querySelector('.receiptContainer');
+				document
+					.getElementById("download")
+					.addEventListener("click", function () {
+						var offScreen = document.querySelector(".receiptContainer");
 
-					window.scrollTo(0, 0);
-					// Use clone with htm2canvas and delete clone
-					html2canvas(offScreen).then((canvas) => {
-						var dataURL = canvas.toDataURL();
-						console.log(dataURL);
-						var link = document.createElement('a');
-						link.download = `${timeRangeSlug}_receiptify.png`;
-						link.href = dataURL;
-						document.body.appendChild(link);
-						link.click();
-						document.body.removeChild(link);
-						delete link;
+						window.scrollTo(0, 0);
+						// Use clone with htm2canvas and delete clone
+						html2canvas(offScreen).then((canvas) => {
+							var dataURL = canvas.toDataURL();
+							console.log(dataURL);
+							var link = document.createElement("a");
+							link.download = `${timeRangeSlug}_receiptify.png`;
+							link.href = dataURL;
+							document.body.appendChild(link);
+							link.click();
+							document.body.removeChild(link);
+							delete link;
+						});
 					});
-				});
 			},
-			error: function(XMLHttpRequest, textStatus, errorThrown) {
+			error: function (XMLHttpRequest, textStatus, errorThrown) {
 				// error handler here
-			}
+			},
 		});
 	}
-
 
 	var params = getHashParams();
 
@@ -94,28 +186,29 @@
 		error = params.error;
 
 	if (error) {
-		alert('There was an error during the authentication');
+		alert("There was an error during the authentication");
 	} else {
 		if (access_token) {
 			$.ajax({
-				url: 'https://api.spotify.com/v1/me',
+				url: "https://api.spotify.com/v1/me",
 				headers: {
-					Authorization: 'Bearer ' + access_token
+					Authorization: "Bearer " + access_token,
 				},
 				success: function (response) {
 					displayName = response.display_name.toUpperCase();
-					$('#login').hide();
-					$('#loggedin').show();
-				}
+					$("#login").hide();
+					$("#loggedin").show();
+				},
 			});
+			loadInitialTracks();
 		} else {
 			// render initial screen
-			$('#login').show();
-			$('#loggedin').hide();
+			$("#login").show();
+			$("#loggedin").hide();
 		}
 
-		document.getElementById('short_term').addEventListener('click', retrieveTracks('short_term', 1, 'LAST MONTH'), false);
-		document.getElementById('medium_term').addEventListener('click', retrieveTracks('medium_term', 2, 'LAST 6 MONTHS'), false);
-		document.getElementById('long_term').addEventListener('click', retrieveTracks('long_term', 3, 'ALL TIME'), false);
+		document.getElementById("short_term").addEventListener("click", () => retrieveTracks("short_term", 1, "LAST MONTH"), false);
+		document.getElementById("medium_term").addEventListener("click", () => retrieveTracks("medium_term", 2, "LAST 6 MONTHS"), false);
+		document.getElementById("long_term").addEventListener("click", () => retrieveTracks("long_term", 3, "ALL TIME"), false);
 	}
 })();
